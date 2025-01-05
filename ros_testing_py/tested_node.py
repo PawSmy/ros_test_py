917780fcb53098ccf6a9bb7c92a5303cd082221f
Pawel <you@example.com>
2025-01-05 17:05:11 +0100

    Pylint fix 8

--- ros_testing_py/tested_node.py
+++ ros_testing_py/tested_node.py
@@ -4,7 +4,6 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Float32MultiArray
 
-
 def calc_vels(msg):
     """Calculates wheel welocities for mecanum drive robot
 
